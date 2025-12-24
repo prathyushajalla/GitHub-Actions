c3720bae6136116caf1c07c1abf16e6edec93f45
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-24 17:06:04 -0500

    Removed the backend config from child module

--- Terraform/Modules/Resource-Group/main.tf
+++ Terraform/Modules/Resource-Group/main.tf
@@ -5,13 +5,6 @@ terraform {
       version = "4.57.0"
     }
   }
-    backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate1766591150"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
-    use_oidc             = true # Required since we're using OIDC in GitHub Actions
-  }
 }
 
 provider "azurerm" {
