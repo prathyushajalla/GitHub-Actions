bda5b654178f3f664478cf2f492dbdf837f6b486
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-24 08:39:24 -0500

    Iac - Azure - Test Deploy RG

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,6 +9,9 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
+  # Terraform requires this block to handle specific behaviors for Azure resources (like resource deletion policies).
+  # Without it, the provider cannot initialize properly during the planning phase
+  features {}
 }
 
 resource "azurerm_resource_group" "Test-RG" {
