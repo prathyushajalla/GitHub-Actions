dc4705627a74e5d58c2693a90f64a6fb1e2fa0c3
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-24 10:36:38 -0500

    Added NSG and Vnet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,3 +18,32 @@ resource "azurerm_resource_group" "Test-RG" {
   name     = "Test-Resource-Group"
   location = "East US"
 }
+
+resource "azurerm_network_security_group" "NSG" {
+  name                = "Network-Security-Group"
+  location            = azurerm_resource_group.Test-RG.location
+  resource_group_name = azurerm_resource_group.Test-RG.name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "virtual-network"
+  location            = azurerm_resource_group.Test-RG.location
+  resource_group_name = azurerm_resource_group.Test-RG.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name             = "subnet1"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "subnet2"
+    address_prefixes = ["10.0.2.0/24"]
+    security_group   = azurerm_network_security_group.NSG.id
+  }
+
+  tags = {
+    environment = "Development"
+  }
+}
