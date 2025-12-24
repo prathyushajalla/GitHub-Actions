4f6a9218dfa15204d0bb2a370a3d45ad45f7f3ec
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-24 08:35:55 -0500

    Iac - Azure - Deploy RG

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,20 +1,17 @@
 terraform {
-  required_version = ">= 1.0.0" # Ensure that the Terraform version is 1.0.0 or higher
-
   required_providers {
-    aws = {
-      source = "hashicorp/aws" # Specify the source of the AWS provider
-      version = "~> 4.0"        # Use a version of the AWS provider that is compatible with version
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.57.0"
     }
   }
 }
 
-/*provider "aws" {
-  region = "us-east-1" # Set the AWS region to US East (N. Virginia)
+provider "azurerm" {
+  # Configuration options
 }
 
-resource "aws_instance" "aws_example" {
-  tags = {
-    Name = "ExampleInstance" # Tag the instance with a Name tag for easier identification
-  }
-}*/
+resource "azurerm_resource_group" "Test-RG" {
+  name     = "Test-Resource-Group"
+  location = "East US"
+}
