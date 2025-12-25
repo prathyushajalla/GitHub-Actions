62453fa9c52298060fab86f4cf947835e16423b7
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-25 15:50:41 -0500

    update to azuread

--- Terraform/Development/Virtual-Network/main.tf
+++ Terraform/Development/Virtual-Network/main.tf
@@ -5,11 +5,12 @@ terraform {
       version = "4.57.0"
     }
   }
+
     backend "azurerm" {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstate1766591150"
     container_name       = "terraform-state"
-    use_oidc             = true # Required since we're using OIDC in GitHub Actions
+    use_azuread_auth     = true # Required since we're using OIDC in GitHub Actions
   }
 }
 
