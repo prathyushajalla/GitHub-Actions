f11a654bbb50e30a291615110cf315bae5d3535d
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-25 15:54:00 -0500

    Verify if fmt is working

--- Terraform/Development/Virtual-Network/main.tf
+++ Terraform/Development/Virtual-Network/main.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-    backend "azurerm" {
+  backend "azurerm" {
     resource_group_name  = "tfstate-rg"
     storage_account_name = "tfstate1766591150"
     container_name       = "terraform-state"
-    use_azuread_auth     = true # Required since we're using OIDC in GitHub Actions
+    use_azuread_auth     = true
   }
 }
 
