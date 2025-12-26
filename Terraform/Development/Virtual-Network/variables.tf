9df207c62b219209322fd7c1fbea392f189d89c2
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-26 16:02:51 -0500

    Removed the unnecessary variables definitions

--- Terraform/Development/Virtual-Network/variables.tf
+++ Terraform/Development/Virtual-Network/variables.tf
@@ -2,14 +2,6 @@ variable "nsgname" {
   type = string
 }
 
-variable "location" {
-  type = string
-}
-
-variable "rgname" {
-  type = string
-}
-
 variable "security_rule_name" {
   type = string
 }
