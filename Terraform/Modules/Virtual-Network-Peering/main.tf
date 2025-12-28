e1c1f15ead60c9b7cf9fe612179c3e030adbe814
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-28 09:12:09 -0500

    Hardcoded to test

--- Terraform/Modules/Virtual-Network-Peering/main.tf
+++ Terraform/Modules/Virtual-Network-Peering/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   name                         = "${var.env}-${var.location}-RG-spoke-to-dev-usc-RG-hub"
   resource_group_name          = "${var.env}-${var.location}-RG"
   virtual_network_name         = var.virtual_network_name.name
-  remote_virtual_network_id    = "dev-usc-vnet".id
+  remote_virtual_network_id    = "/subscriptions/27d7b904-9129-4415-b01d-d5550f6115d5/resourceGroups/dev-centralus-RG/providers/Microsoft.Network/virtualNetworks/dev-centralus-vnet"
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = true  # Enables BGP propagation
