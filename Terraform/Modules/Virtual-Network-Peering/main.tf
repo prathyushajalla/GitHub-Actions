7a3bda6f1451fdd92bc5f5c865f8de95d278ec67
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-28 18:45:34 -0500

    corrected the name mismatch

--- Terraform/Modules/Virtual-Network-Peering/main.tf
+++ Terraform/Modules/Virtual-Network-Peering/main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_virtual_network_peering" "hub_to_spoke" {
-  name                         = "dev-usc-RG-hub-to-${var.env}-${var.location}-RG-spoke" #HUB always has to be Central US for this architecture
-  resource_group_name          = "dev-usc-RG"
+  name                         = "dev-centralus-RG-hub-to-${var.env}-${var.location}-RG-spoke" #HUB always has to be Central US for this architecture
+  resource_group_name          = "dev-centralus-RG"
   virtual_network_name         = "dev-usc-vnet"
   remote_virtual_network_id    = var.remote_virtual_network_id
   allow_virtual_network_access = true
@@ -23,7 +23,7 @@ resource "azurerm_virtual_network_peering" "hub_to_spoke" {
 }
 
 resource "azurerm_virtual_network_peering" "spoke_to_hub" {
-  name                         = "${var.env}-${var.location}-RG-spoke-to-dev-usc-RG-hub"
+  name                         = "${var.env}-${var.location}-RG-spoke-to-dev-centralus-RG-hub"
   resource_group_name          = "${var.env}-${var.location}-RG"
   virtual_network_name         = var.virtual_network_name
   remote_virtual_network_id    = "/subscriptions/27d7b904-9129-4415-b01d-d5550f6115d5/resourceGroups/dev-centralus-RG/providers/Microsoft.Network/virtualNetworks/dev-centralus-vnet"
