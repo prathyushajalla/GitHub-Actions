7f3f1ccb10caae94b220f0b43df52492ad63b7d7
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-28 09:45:43 -0500

    Update main.tf

--- Terraform/Modules/Virtual-Network-Peering/main.tf
+++ Terraform/Modules/Virtual-Network-Peering/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network_peering" "hub_to_spoke" {
 resource "azurerm_virtual_network_peering" "spoke_to_hub" {
   name                         = "${var.env}-${var.location}-RG-spoke-to-dev-usc-RG-hub"
   resource_group_name          = "${var.env}-${var.location}-RG"
-  virtual_network_name         = var.virtual_network_name.name
+  virtual_network_name         = var.virtual_network_name
   remote_virtual_network_id    = "/subscriptions/27d7b904-9129-4415-b01d-d5550f6115d5/resourceGroups/dev-centralus-RG/providers/Microsoft.Network/virtualNetworks/dev-centralus-vnet"
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
