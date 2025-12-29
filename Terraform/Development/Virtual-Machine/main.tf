5f344c307a97951dc0b9a05619a066a6aa649449
Prathyusha Jalla <prathyushajalla4@gmail.com>
2025-12-28 20:04:43 -0500

    Updated main.tf

---
+++ Terraform/Development/Virtual-Machine/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.57.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate1766591150"
+    container_name       = "terraform-state"
+    use_azuread_auth     = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
