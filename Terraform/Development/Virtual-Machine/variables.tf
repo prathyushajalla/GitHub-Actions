bf4f990824d6d389c882a9ff9fb55efae6ebeab7
prathyushajalla <210395129+prathyushajalla@users.noreply.github.com>
2025-12-29 01:02:33 +0000

    style: automated terraform fmt

--- Terraform/Development/Virtual-Machine/variables.tf
+++ Terraform/Development/Virtual-Machine/variables.tf
@@ -7,12 +7,12 @@ variable "location" {
 }
 
 variable "admin_username" {
-  type = string
+  type    = string
   default = "Azure"
 }
 
 variable "admin_password" {
-  type = string
+  type    = string
   default = "Azure@123"
 }
 
