6a1abdc52de502ea8a6025a524778bbcb136a697
Prathyusha Jalla <prathyushajalla4@gmail.com>
2026-01-03 19:35:48 -0500

    Updated the region

--- Terraform/Modules/Private-Link-App/main.tf
+++ Terraform/Modules/Private-Link-App/main.tf
@@ -85,7 +85,7 @@ resource "azurerm_service_plan" "plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "B1" # Lowest tier supporting VNet Integration
+  sku_name            = "F1" # Lowest tier supporting VNet Integration
 }
 
 resource "azurerm_linux_web_app" "webapp" {
