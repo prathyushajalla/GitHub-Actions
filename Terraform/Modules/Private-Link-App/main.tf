78e57bad48fb03deb47b2a01b39f18e0240a9382
Prathyusha Jalla <prathyushajalla4@gmail.com>
2026-01-03 19:45:46 -0500

    Updated the webapp config

--- Terraform/Modules/Private-Link-App/main.tf
+++ Terraform/Modules/Private-Link-App/main.tf
@@ -95,6 +95,7 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
+    always_on              = false
     vnet_route_all_enabled = true # Force all traffic through VNet
   }
 }
